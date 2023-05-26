6c5756262d5972984f23fd88b5472470b19b1ca9
hh hh <abdullah.m1230@gmail.com>
2023-05-26 19:35:25 +0200

    2nd commit

--- vot.sol
+++ vot.sol
@@ -3,12 +3,17 @@ pragma solidity >= 0.7.0 <0.9.0;
 contract Voting {
     mapping(address => bool) public hasVoted;
     uint256 public totalVotes;
+    address[] public votedAddresses;
 
     function vote() public {
         require(!hasVoted[msg.sender], "Already voted.");
         hasVoted[msg.sender] = true;
         totalVotes++;
+        votedAddresses.push(msg.sender);
     }
 
-   
+    function getVotedAddresses() public view returns (address[] memory) {
+        return votedAddresses;
+
+    }
 }
